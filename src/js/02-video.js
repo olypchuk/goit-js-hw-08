ea8713537658436ef7241ab0e82b8143600c8e90
olypchuk <eragmont@gmail.com>
2022-05-27 19:46:04 +0300

    fix 2task console error,3 task fix undefined

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,12 +4,12 @@ const throttle = require('lodash.throttle');
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', throttle(takeValueSeconds,1000));
+const dataOnStart=localStorage.getItem("videoplayer-current-time")
+if (dataOnStart) {
+     player.setCurrentTime(dataOnStart)
+}
+player.on('timeupdate', throttle(takeValueSeconds, 1000));
 
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
-    
 function takeValueSeconds(data) {
      localStorage.setItem("videoplayer-current-time", Number.parseInt(data.seconds));
-     
-
     }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,18 +1,13 @@
 const throttle = require('lodash.throttle');
-const obj = {}
+let obj;
 const form = document.querySelector("form")
-const input =document.querySelector("input")
-const textarea=document.querySelector("textarea")
-    
+  
 form.addEventListener("input",throttle(createData,500));
 form.addEventListener("submit", submitForm);
 
-
- checkInput()
 function createData(e) {
     e.preventDefault()
     obj[e.target.name] = e.target.value;
-
     const json = JSON.stringify(obj);
     localStorage.setItem("feedback-form-state", json)
  
@@ -23,20 +18,52 @@ function submitForm(e) {
     if (obj["email"]) {
      console.log(obj);   
     }
-    
     e.currentTarget.reset()
     localStorage.clear()
-    
+    obj={}
 }
 
-function checkInput() {
-
-    const savedMsg = localStorage.getItem("feedback-form-state");
-   
-    if (savedMsg) {
-         const mail = JSON.parse(savedMsg)["email"];
-        const msg = JSON.parse(savedMsg)["message"];
-        input.value = mail;
-        textarea.value=msg
+function isValue(obj) {
+    if (obj !== '') {
+        for (const key in obj)  {
+               form[key].value = obj[key]; 
+            }  
     }
 }
+
+function loadingPage() {
+    obj = JSON.parse(localStorage.getItem("feedback-form-state"))
+    if (obj) {
+       return isValue(obj)
+    }
+    obj = {}
+    
+}
+loadingPage()
+
+//  checkInput()
+// function checkInput() {
+//     const savedMsg = localStorage.getItem("feedback-form-state");
+
+//     if (savedMsg) {
+//         const mail = JSON.parse(savedMsg)["email"];
+//         const msg = JSON.parse(savedMsg)["message"];
+//         if (mail) {
+//             obj.email = mail;
+//             input.value=mail
+//         }
+//         if (msg) {
+//             obj.message = msg;
+//              textarea.value=msg
+//         }
+        
+//         console.log(obj);
+        
+//         const updateObj = JSON.stringify(obj);
+//         localStorage.setItem("feedback-form-state", updateObj)
+//     }
+// }
+     
+              
+          
+             
